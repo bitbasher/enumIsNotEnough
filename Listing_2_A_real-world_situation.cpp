e320d861159ba926d8aa4736f1e3a2247239dc2a
Jeff Hayes <vulcan_@mac.com>
2025-11-11 23:19:34 +0100

    adding Listing two .. a real c++ bit of source

---
+++ Listing_2_A_real-world_situation.cpp
@@ -0,0 +1,14 @@
+Listing 2: A real-world situation
+enum Sounds {
+  // Human sounds in [100, 200[
+  explosionSnd = 101,
+  attackSnd = 102,
+  // ricochetSnd = 103, // removed, beta testers hated it
+  deathSnd = 104,
+  // Alien sounds in [200, 300[
+  alienAttackSnd = 201,
+  ...
+  alienDeathSnd = 227,
+  // Miscellaneous sounds >= 500
+  victorySnd = 500
+};
