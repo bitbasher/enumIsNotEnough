7db1a9a3e9a25b20d4341e6613e958b3e5344a40
Jeff Hayes <vulcan_@mac.com>
2025-11-11 23:38:51 +0100

    Add Test_Enum_String implementation

---
+++ Test_Enum_String.cpp
@@ -0,0 +1,9 @@
+#include "Test_Enum_String.hpp"
+Tmpl_Enum<std::string, Test_Enum_String>::instances_list 
+Tmpl_Enum<std::string, Test_Enum_String>::s_instances;
+
+const Test_Enum_String Test_Enum_String::enum_Alpha("Alpha");
+const Test_Enum_String Test_Enum_String::enum_Beta("Beta");
+const Test_Enum_String Test_Enum_String::enum_Delta("Delta");
+const Test_Enum_String Test_Enum_String::enum_Epsilon("Epsilon");
+const Test_Enum_String Test_Enum_String::enum_Omega("Omega");
