1ae5f667d35d004fc7dfa11e5e57f34a778616ca
Jeff Hayes <vulcan_@mac.com>
2025-11-11 23:39:56 +0100

    Add main function to validate Test_Enum_String elements
    This file contains a main function that validates string elements against an enum and displays their values.

---
+++ Test_Enum_String_Main.cpp
@@ -0,0 +1,45 @@
+#include "Test_Enum_String.hpp"
+
+void Validate_Str_Elem(const std::string& theString)
+{
+  using std::cout;
+  using std::endl;
+  cout << theString;
+  if ( Test_Enum_String::Is_Valid_Value(theString) )
+    cout << " is ";
+  else
+    cout << " isn't ";
+  cout << "a valid value for Test_Enum_String." << endl;
+}
+
+int main()
+{
+  using std::cout;
+  using std::endl;
+  int Cur_Elem = 0;
+  for ( Test_Enum_String::const_iterator
+        i = Test_Enum_String::begin();
+        i != Test_Enum_String::end(); ++i )
+  {
+    Cur_Elem++;
+    cout << "Test_Enum_String element #" << Cur_Elem << " value = " << (*i)->Get_Value() << endl;
+  }
+
+  cout << "Total #elements = " << Test_Enum_String::size() << endl;
+
+  cout << "Min enum value = " << Test_Enum_String::Min() << endl;
+  cout << "Max enum value = " << Test_Enum_String::Max() << endl;
+
+  Validate_Str_Elem("Alpha");
+  Validate_Str_Elem("Gamma");
+  Validate_Str_Elem("Beta");
+  Validate_Str_Elem("BetA");
+
+  Test_Enum_String E(Test_Enum_String::enum_Alpha);
+  cout << "Value of E = " << E.Get_Value() << endl;
+
+  E = Test_Enum_String::enum_Omega;
+  cout << "Value of E = " << E.Get_Value() << endl;
+  }
+  return 0;
+}
